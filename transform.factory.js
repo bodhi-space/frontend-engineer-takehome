dd14e5336943f481dba92a5c9568dd15af62f3d5
Daniel Olasky <daniel.olasky@hotschedules.com>
2016-08-08 20:39:45 -0700

    finishing touches

--- transform.factory.js
+++ transform.factory.js
@@ -3,6 +3,8 @@
         console.log('factory working')
         //your code goes here, this return statement is just a placeholder to 
         // avoid angular error
+        // return a function or functions to transform the data as specified in the 
+        // README.md
         return {};
     }
     
--- visualize.directive.js
+++ visualize.directive.js
@@ -9,6 +9,9 @@
             controller: ['$scope', 'transformFactory', function($scope, transformFactory) {
                 console.log('directive working') 
                 // add your directive functionality here 
+                // you  should pass in the raw SalesTransactions from the controller in scope
+                // then call the transformFactory function or functions to transform the data
+                // then pass it to the directive template (which you should also edit)
             }]
         }
     }
