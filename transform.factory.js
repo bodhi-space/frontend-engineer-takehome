0b526fef855443f82cd21606057bda1111e5713a
Sepand Assadi <contact@sepandassadi.com>
2016-08-13 17:27:44 -0700

    add assign 1 + style using flexbox

--- transform.factory.js
+++ transform.factory.js
@@ -1,12 +1,62 @@
-(function() {
-    function factory() {
-        console.log('factory working')
-        //your code goes here, this return statement is just a placeholder to 
-        // avoid angular error
-        // return a function or functions to transform the data as specified in the 
-        // README.md
-        return {};
+var app = angular.module('takeHomeApp', []);
+
+app.controller('TakeHomeController', [
+  '$scope',
+  '$http',
+
+  function($scope, $http){
+    console.log('TakeHome Started');
+
+    //  Requesting all data from our JSON Data file
+    $http.get('SalesTransactions.json')
+      .then(function(response) {
+        // console.log('response', response);
+
+        // 1) net_total for the entire collection
+        $scope.NetTotal = calculateNetTotal(response.data);
+
+        // 2) average net_total per guest for each employee
+        $scope.avgNetTotalPerEmployee = calculateAvgTotalPerEmployee(response.data);
+        // $scope.SalesTransactionData =
+      });
+  }]);
+
+function calculateNetTotal(data){
+  // Using helper function to extract
+  // pieces we want from our JSON file
+  var allValues = extractData(data, 'net_total'),
+      total = 0;
+
+  // Calculating the net_total of all orders
+  for(var prop in allValues){
+    total += allValues[prop].value;
+  }
+  return total;
+}
+
+function calculateAvgTotalPerEmployee(data){
+
+}
+
+function extractData(data, type){
+  var result = [];
+
+  for(var prop in data){
+    // make sure we only extract data
+    // equal to the type
+    if(data[prop][type]){
+      result.push(data[prop][type]);
     }
-    
-    angular.module('takeHomeApp').factory('transformFactory', factory)
-})();
+  }
+
+  return result;
+}
+
+
+
+
+
+
+
+
+
