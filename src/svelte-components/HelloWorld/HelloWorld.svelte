ff6196df9975e24b4b2a18906f1b0127c1decd21
Uzair Rahim <uzair.rahim@hotschedules.com>
2017-02-10 10:57:48 -0600

    CSS, stateful react component and counter

--- src/svelte-components/HelloWorld/HelloWorld.svelte
+++ src/svelte-components/HelloWorld/HelloWorld.svelte
@@ -1,1 +1,17 @@
-<h1>Hello {{name}}</h1>
+<style>
+	h1,h2 {
+		font-family: 'Roboto Slab';
+		color: #4a5456;
+	}
+
+	h1 {
+		margin: 0;
+	}
+
+	h2 {
+		margin-top: 0;
+	}
+</style>
+
+<h1>Hello World</h1>
+<h2>Count: {{count}}</h2>
