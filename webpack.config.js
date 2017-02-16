6b7f9257a10dc3dfc5e38ac39617eb09104bcd3b
tiffany.lee <tiffany.lee@hotschedules.com>
2017-02-16 12:19:03 -0800

    [update] webpack.config.js: (DASH-292) add configs for modal

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,71 @@
-module.exports = [
-	// {
-	// 	entry: './src/index.js',
-	// 	output: {
-	// 		filename: './dist/bundle.js',
-	// 		library: ['MyLib'],
-	// 		libraryTarget: 'umd'
-	// 	}
-	// },
-	{
-		name: 'modal',
-		entry: './src/react-components/Modal/Modal.api.js',
-		output: {
-			filename: './dist/modal_bundle.js',
-			library: ['Modal'],
-			libraryTarget: 'umd'
-		},
-	}
-];
+(function() {
+
+    'use strict';
+
+    var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+    module.exports = [
+        // {
+        // 	entry: './src/index.js',
+        // 	output: {
+        // 		filename: './dist/bundle.js',
+        // 		library: ['MyLib'],
+        // 		libraryTarget: 'umd'
+        // 	}
+        // },
+        {
+            name: 'modal',
+            entry: './src/react-components/Modal/Modal.api.js',
+            output: {
+                filename: './dist/modal_bundle.js',
+                library: ['Modal'],
+                libraryTarget: 'umd'
+            },
+            module: {
+                loaders: [
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        loader: "babel-loader"
+                    },
+                    {
+                        test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+                        loader: "file"
+                    },
+                    {
+                        test: /\.css$/,
+                        exclude: /node_modules/,
+                        use: [
+                            {
+                                loader: 'style-loader'
+                            },
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    modules: true,
+                                    localIdentName: 'echo-component-[name]-[hash:base64:5]'
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        },
+
+        {
+            name: 'modal_container',
+            entry: './src/modal_container.js',
+            output: {
+                filename: './dist/index.js',
+                libraryTarget: 'umd'
+            },
+            plugins: [
+                new CopyWebpackPlugin([
+                    { from: './src/modal_container.html', to: './dist/index.html' }
+                ])
+            ]
+        }
+
+    ];
+
+})();
