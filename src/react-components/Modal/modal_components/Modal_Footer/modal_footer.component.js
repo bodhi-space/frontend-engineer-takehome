bde3fedc6bc017fa1e0285fd95b3d60334cbbac2
tiffany.lee <tiffany.lee@hotschedules.com>
2017-02-21 16:04:21 -0800

    [update] modal: (DASH-292) have modal uptake new button component

--- src/react-components/Modal/modal_components/Modal_Footer/modal_footer.component.js
+++ src/react-components/Modal/modal_components/Modal_Footer/modal_footer.component.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ThemeHOCFactory from '../../../ThemeHOC/Theme.HOC.js';
-import ModalButtonComponent from '../Modal_Button/modal_button.component.js';
+import ButtonComponent from '../../../Button/Button.component.js';
 import styles from './_modal_footer.scss';
 
 const ModalFooterComponent = ( {footerClassName, confirm_text, cancel_text, handleClickConfirm, handleClickCancel, theme} ) => {
 
-	let ThemedButtonComponent = ThemeHOCFactory(theme)(ModalButtonComponent);
+	let ThemedButtonComponent = ThemeHOCFactory(theme)(ButtonComponent);
 
 	return (
 		<div className={`${footerClassName} ${styles.modalFooter}`}>
@@ -14,11 +14,11 @@ const ModalFooterComponent = ( {footerClassName, confirm_text, cancel_text, hand
 				onClick={handleClickConfirm}
 				text={confirm_text}>
 			</ThemedButtonComponent>
-			<ModalButtonComponent 
+			<ButtonComponent 
 				buttonClassName={styles.cancelButton}
 				onClick={handleClickCancel}
 				text={cancel_text}>
-			</ModalButtonComponent>
+			</ButtonComponent>
 		</div>
 
 	);
